fa5fd37eb004b5b802abf93f35179462c901b87d
BGenR14 <62076844+BGenR14@users.noreply.github.com>
2020-10-04 00:07:04 -0500

    1 - 10 Python Patters

---
+++ Python_Patterns.py
@@ -0,0 +1,93 @@
+print("Number 1")
+print()
+n = 10 # Can also be entered from user too
+for i in range(1, n + 1):
+	print("*"*n)
+
+print()
+print("Number 2")
+print()
+
+n = 9 # Can also be entered from user too
+for i in range(1, n + 1):
+	for j in range(1, n + 1):
+		print(i, end = " ")
+	print()
+
+print()
+print("Number 3")
+print()
+
+n = 10 # Can also be entered from user too
+for i in range(1, n + 1):
+	for j in range(1, n + 1):
+		print(j, end = " ")
+	print()
+
+print()
+print("Number 4")
+print()
+
+for i in range(1, n + 1):
+	for j in range(1, n + 1):
+		print(chr(64 + i), end = " ")
+	print()
+
+print()
+print("Number 5")
+print()
+
+for i in range(1, n + 1):
+	for j in range(1, n + 1):
+		print(chr(64 + j), end = " ")
+	print()
+
+print()
+print("Number 6")
+print()
+
+for i in range(1, n + 1):
+	for j in range(1, i + 1):
+		print("*", end = " ")
+	print()
+
+print()
+print("Number 7")
+print()
+
+n = 9
+for i in range(1, n + 1):
+	for j in range(1, i + 1):
+		print(i, end = " ")
+	print()
+
+
+print()
+print("Number 8")
+print()
+
+n = 10
+for i in range(1, n + 1):
+	for j in range(1, i + 1):
+		print(j, end = " ")
+	print()
+
+
+print()
+print("Number 9")
+print()
+
+for i in range(1, n + 1):
+	for j in range(1, i + 1):
+		print(chr(64 + i), end = " ")
+	print()
+
+print()
+print("Number 10")
+print()
+
+for i in range(1, n + 1):
+	for j in range(1, i + 1):
+		print(chr(64 + j), end = " ")
+	print()
+
